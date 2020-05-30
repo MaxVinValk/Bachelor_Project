698111b6f896b8e141415515f90099e4e56cb5fe
Max Valk <maxvalk@pop-os.localdomain>
2020-05-30 14:40:53 +0200

    Added a utility script which converts raw number of guesses out to the accuracy used in the paper. Plus, some standard averaging code

--- utils/transform.py
+++ utils/transform.py
@@ -1,4 +1,4 @@
-def getAveragedAcc(results):
+def getAveragedAccOld(results):
 	#To cumulative
 	cumRes = []
 	for i in range(0, len(results)):
@@ -7,16 +7,16 @@ def getAveragedAcc(results):
 		for j in range(0, len(results[i])):
 			counter += results[i][j]
 			cumTemp.append(counter)
-				
+
 		cumRes.append(cumTemp)
 
 	#To cumulative averaged
-	
+
 	cumAvg = [0] * len(cumRes[i])
 	for i in range(0, len(cumRes)):
 		for j in range(0, len(cumRes[i])):
 			cumAvg[j] += cumRes[i][j]
-	
+
 	for i in range(0, len(cumAvg)):
 		cumAvg[i] /= len(cumRes)
 
@@ -25,5 +25,42 @@ def getAveragedAcc(results):
 	cumAcc = [0] * len(cumAvg)
 	for i in range(0, len(cumAvg)):
 		cumAcc[i] = (i+1) / cumAvg[i]
-	
+
 	return cumAcc
+
+
+def convertGuessesToAccuracy(run):
+	acc = []
+	correct = 0
+
+	for i in range(0, len(run)):
+		if run[i] == 1:
+			correct += 1
+		acc.append(correct / (i+1))
+
+	return acc
+
+def convertSetOfRuns(runs):
+	converted = []
+	for run in runs:
+		converted.append(convertGuessesToAccuracy(run))
+
+	return converted
+
+#Assumes all runs are of equal length
+def averageRuns(runs):
+	runLength = len(runs[0])
+	avg = [0] * runLength
+
+	for run in runs:
+		if len(run) != runLength:
+			print("Issue: Not all runs of the same length")
+			return None
+
+		for i in range(0, len(run)):
+			avg[i] += run[i]
+
+	for i in range(0, len(avg)):
+		avg /= runLength
+
+	return avg
